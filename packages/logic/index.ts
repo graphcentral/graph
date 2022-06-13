db90e6642080e22fce7d731e3ea76cd07aa90668
9oelM <hj923@hotmail.com>
2022-06-13 17:32:50 +0900

    mjsworkin

--- packages/logic/index.ts
+++ packages/logic/index.ts
@@ -1,7 +1,6 @@
 // import { Client } from "@notionhq/client"
 import * as dotenv from "dotenv"
 import path from "path"
-import { separateIdWithDashSafe } from "./official/notion-util"
 import { NotionGraph } from "./unofficial/get-graph-from-root-block"
 import { NotionAPI } from "notion-client"
 import { dirname } from "path"
@@ -10,35 +9,8 @@ import { fileURLToPath } from "url"
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 dotenv.config({ path: path.resolve(__dirname, `..`, `..`, `.env`) })
-
-// const notion = new Client({
-//   auth: process.env.NOTION_TOKEN,
-//   notionVersion: `2022-02-22`,
-// })
 ;(async () => {
   const notionUnofficialClient = new NotionAPI()
-
-  const recordMap2 = await notionUnofficialClient.getPage(
-    `1f96a097fd1a4c53a3c42a3288f39e9d`
-  )
-  separateIdWithDashSafe(`1f96a097fd1a4c53a3c42a3288f39e9d`)
   const notionGraph = new NotionGraph(notionUnofficialClient)
   notionGraph.getGraphFromRootBlock(`1f96a097fd1a4c53a3c42a3288f39e9d`)
-  // this only returns the ids of contents inside the block, which is not what we want
-  // const recordMap3 = await notionUnofficialClient.getBlocks([
-  //   separateIdWithDashSafe(`1f96a097fd1a4c53a3c42a3288f39e9d`),
-  // ])
-  // this returns too much information (all detailed paragraphs, ...)
-  // const recordMap4 = await notionUnofficialClient.getPageRaw(
-  //   `1f96a097fd1a4c53a3c42a3288f39e9d`
-  // )
-  // console.log(JSON.stringify(recordMap2, null, 2))
-  // console.log(JSON.stringify(recordMap2, null, 2))
-  // console.log(JSON.stringify(recordMap4, null, 2))
-
-  // const graph = await getGraphFromRootBlock(
-  //   notion,
-  //   `aa362e29a8c24d6ba084ceca5a717db6`
-  // )
-  // console.log(JSON.stringify(graph, null, 2))
 })()
--- packages/logic/tsup.config.ts
+++ packages/logic/tsup.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "tsup"
 import path from "path"
+import * as dotenv from "dotenv"
+
+dotenv.config({ path: path.resolve(__dirname, `..`, `..`, `.env`) })
 
 export default defineConfig({
   outDir: path.resolve(`.`, `dist`),
-  // minify: true,
+  minify: process.env.NODE_ENV === `production`,
   // enable compile time type checking
   dts: true,
   target: `node16`,
